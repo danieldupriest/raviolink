7ae282e5694c313e4819dd4e21387a0930f7cc81
Daniel Dupriest <kououken@gmail.com>
2022-04-23 16:17:35 -0700

    Set up mustache and basic link view.

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
-const app = express();
+const mustacheExpress = require("mustache-express");
+
 const port = 8080;
 
+const app = express();
+
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "mustache");
+app.engine("mustache", mustacheExpress());
 app.use(express.static("public"));
+
+app.get("/:linkCode", (req, res) => {
+    const data = {
+        appTitle: "Raviolink",
+        content: req.params.linkCode,
+    };
+    res.render("linkView", data);
+});
 
 app.get("/", (req, res) => {
     res.send("Home page");
