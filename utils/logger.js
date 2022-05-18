3c82f13afcf10c20c737b204a8d0272651757a40
Daniel Dupriest <kououken@gmail.com>
2022-05-18 12:34:13 -0700

    Implemented logging of requests.

--- utils/logger.js
+++ utils/logger.js
@@ -1,4 +1,8 @@
+const fs = require("fs");
+const logFile = fs.createWriteStream("./debug.log", { flags: "w" });
+
 module.exports = function (req, res, next) {
+    logFile.write("Handling request to: " + req.url);
     console.log("Handling request to: " + req.url);
     console.log("  With Params: " + JSON.stringify(req.params));
     console.log("  With Body: " + JSON.stringify(req.body));
