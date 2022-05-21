c4a52bac07589925238c4d52d5888fa089fd8bb6
Daniel Dupriest <kououken@gmail.com>
2022-05-20 17:24:30 -0700

    Added newline to log entries.

--- utils/logger.js
+++ utils/logger.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const logFile = fs.createWriteStream("./debug.log", { flags: "w" });
 
 module.exports = function (req, res, next) {
-    logFile.write("Handling request to: " + req.url);
+    logFile.write("Handling request to: " + req.url + "\n");
     console.log("Handling request to: " + req.url);
     console.log("  With Params: " + JSON.stringify(req.params));
     console.log("  With Body: " + JSON.stringify(req.body));
